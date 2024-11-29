ecfb8b54dac4e785e2732c3d767c51db9b6e8436
Mikhail Mamontov <mmontovmm@gmail.com>
2024-11-30 03:39:00 +1000

    init docker

---
+++ backend/src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+@app.get('/')
+async def test_route():
+    return {"response": "Hello world!"}
+
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
