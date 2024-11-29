b3c05f2f543b8e955e3337ab87a86a28e15fc3cc
Mikhail Mamontov <mmontovmm@gmail.com>
2024-11-30 04:35:02 +1000

    edit main

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,8 +8,8 @@ async def test_route():
     return {"response": "Hello world!"}
 
 origins = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     
 ]
 
