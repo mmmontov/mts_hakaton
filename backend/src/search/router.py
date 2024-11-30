a8e34974485d5a2579780540e8292887c7ce1171
Mikhail Mamontov <mmontovmm@gmail.com>
2024-12-01 02:32:04 +1000

    fix json function

--- backend/src/search/router.py
+++ backend/src/search/router.py
@@ -11,17 +11,11 @@ router = APIRouter(
     tags=['search']
 )
 
-# def get_next_level(target_col, prev_col, check_prev):
-#     query = eval(f'select(workers.c.{target_col}).where(workers.c.{prev_col} == "{check_prev}").distinct()')
-#     return query
-
-# def compare_row_to_list(answer):
-#     # return list(map(lambda x: x[0], answer.all()))
-#     return [i[0] for i in answer.all()]
 
 async def generate_nested_structure(session: AsyncSession):
     # Выполняем запрос для получения всех данных
     query = select(
+        workers.c.id,
         workers.c.department_1,
         workers.c.functional_block,
         workers.c.department_2,
@@ -54,7 +48,7 @@ async def generate_nested_structure(session: AsyncSession):
         
         # Добавляем сотрудника на текущем уровне вложенности
         worker = {
-            "id": len(current["workers"]) + 1,  # Пример ID, если нет уникального ID
+            "id": row.id,  # Используем pk (id) из базы данных
             "name": row.name,
             "surname": row.surname,
             "flag": row.employment,
@@ -77,81 +71,4 @@ async def get_nested_workers(session: AsyncSession = Depends(get_async_session))
     return nested_data
 
 
-
-# @router.get('/test')
-# async def test(session: AsyncSession = Depends(get_async_session)):
-#     return generate_nested_structure(session)
-    
-    
-
-# @router.get('/')
-# async def test_route(session: AsyncSession = Depends(get_async_session)):
-#     block_example = {
-#         "level": None,
-#         "name": None,
-#         "sub_departments": [],
-#         "workers": []
-#     }
-    
-    
-#     response = []
-
-#     query = select(workers.c.department_1).distinct()
-#     answer = await session.execute(query)
-#     result = compare_row_to_list(answer)
-    
-#     for department_1 in result:
-#         department_block = {
-#         "level": None,
-#         "name": None,
-#         "sub_departments": [],
-#         "workers": []
-#     }
-#         department_block["level"] = "department_1"
-#         department_block["name"] = department_1
-        
-#         query = get_next_level('functional_block', 'department_1', department_1)
-#         answer = await session.execute(query)
-#         result = compare_row_to_list(answer)
-        
-#         for functional_block in result:
-#             if functional_block:
-#                 query = select(workers.c.department_2).where(
-#                     (workers.c.department_1 == department_1) &
-#                     (workers.c.functional_block == functional_block)).distinct()
-#                 answer = await session.execute(query)
-                
-#                 # print(compare_row_to_list(answer))
-#                 for department_2 in answer:
-#                     if department_2:
-#                         ...
-                    
-                
-#             else: # добавляем председателя департамента       
-#                 query = select(workers.c.id, 
-#                             workers.c.job_title,
-#                             workers.c.name,
-#                             workers.c.surname,
-#                             workers.c.employment).where(
-#                                 (workers.c.department_1 == department_1) &
-#                                         (workers.c.functional_block == None))
-#                 # print(department_block)
-#                 answer = await session.execute(query)
-#                 answer = answer.all()[0]
-#                 # print(answer)
-#                 worker = {
-#                     "id": answer[0],
-#                     "job_title": answer[1],
-#                     "name": answer[2],
-#                     "surname": answer[3],
-#                     "employment": answer[4],
-#                 }
-#                 # dep_workers = department_block['workers'] 
-#                 # dep_workers.append(worker)
-#                 department_block['workers'].append(worker)
-                
-        
-#         response.append(department_block)
-        
-#     return response
         
